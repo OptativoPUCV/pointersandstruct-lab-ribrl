e35a0be4d5119e8ce4defda24de2f37912e5ec9e
Replit user <replituser@example.com>
2021-03-24 00:22:52 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -122,6 +122,6 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
 	c->datos[0]= a1 + a2;
 	c->datos[1]= b1 + b2;
-	printf("resutado1%i",c->datos[0]);
-	printf("resutado1%i",c->datos[1]);
+	printf("resutado1 = %i",c->datos[0]);
+	printf("resutado2 = %i",c->datos[1]);
 }
